07a72a7356f0f51d8f1d0795de51b53277aeb85b
David Mizrahi <davidvoodo@gmail.com>
2025-08-24 12:06:39 +0300

    feat: print my name (דויד מזרחי)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 
-def main():
-    print("Welcome to Git Assignment")
-    print("Current date and time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("Welcome to Git Assignment")
 
-if __name__ == "__main__":
-    main()
+print("My name is: דויד מזרחי")
